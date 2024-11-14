a85de05bbb435c61645d9be2d878fb705d1ee658
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-11-14 21:17:59 +0100

    rewrite all

--- hopfield-network-visualization/src/app/page.tsx
+++ hopfield-network-visualization/src/app/page.tsx
@@ -1,22 +1,16 @@
 "use client";
 
 import React, { useState } from 'react';
-import Grid from "@/components/Grid";
 
 export default function Home() {
-  const [patterns, setPatterns] = useState<number[][]>([]);
-
-  const handleSavePattern = (newPattern: number[]) => {
-    setPatterns([...patterns, newPattern]);
-  };
 
   return (
     <div className="flex flex-col items-center">
       <div className="w-9/12 sm:w-96 border-2 mb-4 aspect-square">
-        <Grid onSavePattern={handleSavePattern} />
+
       </div>
       <div>
-        {/* Additional content can go here */}
+
       </div>
     </div>
   );
